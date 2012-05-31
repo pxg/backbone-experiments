93699f87d3d18b9e4f96793c90bc14722a0e60f0
Pete Graham <petegraham1@gmail.com>
2012-05-31 22:05:48 +0100

    working with backend

---
+++ data/favourite_q_2012-05-31.sql
@@ -0,0 +1,61 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.9-log)
+# Database: favourite_q
+# Generation Time: 2012-05-31 21:05:25 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table questions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `questions`;
+
+CREATE TABLE `questions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `question` varchar(140) DEFAULT NULL,
+  `asked` timestamp NULL DEFAULT NULL,
+  `created_on` timestamp NULL DEFAULT NULL,
+  `updated_on` timestamp NULL DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+
+INSERT INTO `questions` (`id`, `question`, `asked`, `created_on`, `updated_on`, `weight`)
+VALUES
+	(1,'What\'s your favourite cheese?',NULL,'2012-05-31 21:00:11','2012-05-31 21:00:14',0),
+	(2,'What\'s your favourite animal?','2012-05-31 21:02:18','2012-05-31 21:00:55','2012-05-31 21:01:04',1),
+	(3,'What\'s your favourite colour?',NULL,'2012-05-31 21:00:57','2012-05-31 21:01:07',1),
+	(4,'Who\'s your favourite Boooth?',NULL,'2012-05-31 21:00:59','2012-05-31 21:01:11',2),
+	(6,'What\'s your favourite sandwich?',NULL,NULL,NULL,NULL),
+	(7,'Who\'s your favourite Mike?',NULL,NULL,NULL,NULL),
+	(8,'What\'s your favourite plant?',NULL,NULL,NULL,NULL);
+
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
